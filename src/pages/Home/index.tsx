75fccaf761ad92959466ba582659236c3934daff
Rodrigo Deganutti <56453875+rodridega@users.noreply.github.com>
2023-12-05 16:38:38 -0300

    move assets to public

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import { MainLayout } from "../../layouts"
-import base from '../../assets/images/00. Base.png'
+import base from '/assets/images/00. Base.png'
 import { Outlet } from "react-router-dom"
 import { useContext } from 'react'
 import { ImagenesContext } from "../../context/ImagenesContext"
@@ -15,11 +15,11 @@ export const Home = () => {
             <div className='w-10/12 '>
                 <div className='relative'>
                     <img src={base} alt='' className='w-full absolute z-10' />
-                    <img src={`../../assets/images/Pisos/${imagenes.pisos}.png`} alt='' className='w-full absolute z-20' />
-                    <img src={`../../assets/images/Muros/${imagenes.muros}.png`} alt='' className='w-full absolute z-20' />
-                    <img src={`../../assets/images/Salpicaderos/${imagenes.salpicaderos}.png`} alt='' className='w-full absolute z-20' />
-                    <img src={`../../assets/images/Mueble Alto/${imagenes.mueblealto}.png`} alt='' className='w-full absolute z-20' />
-                    <img src={`../../assets/images/Mueble Bajo/${imagenes.mueblebajo}.png`} alt='' className='w-full absolute z-20' />
+                    <img src={`/assets/images/Pisos/${imagenes.pisos}.png`} alt='' className='w-full absolute z-20' />
+                    <img src={`/assets/images/Muros/${imagenes.muros}.png`} alt='' className='w-full absolute z-20' />
+                    <img src={`/assets/images/Salpicaderos/${imagenes.salpicaderos}.png`} alt='' className='w-full absolute z-20' />
+                    <img src={`/assets/images/Mueble Alto/${imagenes.mueblealto}.png`} alt='' className='w-full absolute z-20' />
+                    <img src={`/assets/images/Mueble Bajo/${imagenes.mueblebajo}.png`} alt='' className='w-full absolute z-20' />
                 </div>
             </div>
         </MainLayout>
